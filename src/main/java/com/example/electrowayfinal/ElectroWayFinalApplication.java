f73bee1c6391e6df0336720eaa82811a5fe67547
cosminrotariu <cosmin.rotariu@info.uaic.ro>
2021-04-13 23:33:14 +0300

    fixed verification mail

---
+++ src/main/java/com/example/electrowayfinal/ElectroWayFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.electrowayfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroWayFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectroWayFinalApplication.class, args);
+    }
+
+}
