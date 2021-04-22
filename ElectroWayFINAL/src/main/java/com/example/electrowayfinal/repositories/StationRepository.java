b0a1c44b31cfa9189e3a70a5827339bc56c117d5
iulian2000-cmyk <iuliangeorge581@yahoo.ro>
2021-04-22 20:24:47 +0300

    Station repo

---
+++ ElectroWayFINAL/src/main/java/com/example/electrowayfinal/repositories/StationRepository.java
@@ -0,0 +1,15 @@
+package com.example.electrowayfinal.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.electrowayfinal.models.Station;
+import com.example.electrowayfinal.models.User;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public interface StationRepository extends JpaRepository<Station,Long>
+{
+    Station findByUserID(User user);
+    void deleteStation();
+    void updateStation();
+
+}
