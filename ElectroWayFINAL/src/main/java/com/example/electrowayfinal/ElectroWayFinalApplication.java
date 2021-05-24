8f49aace786fbd2e1307b634d2b6676a29c9f93f
cosminrotariu <cosmin.rotariu@info.uaic.ro>
2021-05-24 13:01:22 +0300

    update

--- ElectroWayFINAL/src/main/java/com/example/electrowayfinal/ElectroWayFinalApplication.java
+++ ElectroWayFINAL/src/main/java/com/example/electrowayfinal/ElectroWayFinalApplication.java
@@ -2,8 +2,10 @@ package com.example.electrowayfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ElectroWayFinalApplication {
 
     public static void main(String[] args) {
