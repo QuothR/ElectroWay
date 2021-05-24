a6aba60307b443d5c86962e972ac5d205d151c25
andrei.ciobanu601@gmail.com <Andrei.elock.123>
2021-05-24 10:24:51 +0300

    added status to charging plug active/inactive

--- src/main page/Content/AdmStatiiContent/EditareStatie/TabelPlugs.js
+++ src/main page/Content/AdmStatiiContent/EditareStatie/TabelPlugs.js
@@ -48,12 +48,14 @@ function TabelPlugs(props) {
 
 
     const renderPlugs = (plug, index) => {
+        const status = plug.status === 1 ? "inactive" : "active"
         return (
             <tr key={index}>
-                <td style={{ width: "29%" }}>Plug-ul numarul {index + 1}</td>
+                <td style={{ width: "30%" }}>Plug-ul numarul {index + 1}</td>
+                <td style={{ width: "14%" }}>{status}</td>
                 <td style={{ width: "24%" }}>{plug.connectorType}</td>
                 <td style={{ width: "15%" }}>{plug.priceKw}</td>
-                <td style={{ width: "10%" }}>{plug.chargingSpeedKw}</td>
+                <td style={{ width: "11%" }}>{plug.chargingSpeedKw}</td>
                 <td style={{ width: "5%" }}
                     onClick={(e) => {
                         sessionStorage.setItem('plugAddr', JSON.stringify(plug));
@@ -90,7 +92,8 @@ function TabelPlugs(props) {
                         <thead>
                             <tr>
                                 <th style={{ width: "30%" }}>Numar Plug</th>
-                                <th style={{ width: "25%" }}>Tip Conector</th>
+                                <th style={{ width: "14%" }}>Status</th>
+                                <th style={{ width: "23%" }}>Tip Conector</th>
                                 <th style={{ width: "15%" }}>Pret</th>
                                 <th style={{ width: "10%" }}>Viteza</th>
                                 <th style={{ width: "5%" }}><GrEdit /></th>
