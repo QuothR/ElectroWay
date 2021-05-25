c48884d9ae8a3569d5617018392b81e5329e6c3b
Curca Andrei <curcaandrei99@gmail.com>
2021-05-25 16:50:19 +0300

    Revert "added SWAGGER-UI"
    This reverts commit c3688cdbe01628e1b6b5743c766b50e228d1d802.

--- ElectroWayFINAL/src/main/java/com/example/electrowayfinal/ElectroWayFinalApplication.java
+++ ElectroWayFINAL/src/main/java/com/example/electrowayfinal/ElectroWayFinalApplication.java
@@ -2,10 +2,8 @@ package com.example.electrowayfinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class ElectroWayFinalApplication {
 
     public static void main(String[] args) {
