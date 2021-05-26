65747846df6404841e4348de0712e5c6b7987718
Antonio Mihăeș <antnm@protonmail.com>
2021-05-27 00:41:56 +0300

    Use light mode

--- ElectroWayMobile/app/src/main/java/com/github/electroway/MainActivity.kt
+++ ElectroWayMobile/app/src/main/java/com/github/electroway/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.electroway
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
